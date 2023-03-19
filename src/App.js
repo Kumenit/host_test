f82ce853dbfa24da8f06a5ded06030bfb8969d9b
kumenit <kumenitdesta082@gmail.com>
2023-03-19 11:18:14 +0300

    changed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  const [number,setnumber]=React.useState(0);
+ function add()
+ {
+  setnumber(setnumber =>setnumber+1)
+ }
+ function minus()
+ {
+  setnumber(setnumber =>setnumber-1)
+ }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+       <div className='minus' onClick={minus}>
+              <b className='text'> minus</b>
+       </div>
+       <div className='number'>
+        <b className='text3'>{number}</b>
+       </div>
+       <div className='plus' onClick={add}>
+        <b className='text'>Plus</b>
+       </div>
+       
     </div>
   );
 }
